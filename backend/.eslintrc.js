fa17f33ed45beb9b523bfd2a9893fda748fa4ed3
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-07-20 00:07:27 -0300

    build: Add db dependencies and .env.example

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,29 +3,27 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  
+
   parser: '@typescript-eslint/parser',
-  
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  
+
   plugins: [
     '@typescript-eslint',
     'import',
   ],
 
- 
+
   extends: [
-    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  
+
   rules: {
-    'no-console': 'warn',
     'import/prefer-default-export': 'off',
   },
 };
